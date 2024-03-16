9c8465e3e2d9b137e09d3e80435dd6966fa17ec0
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-03-16 00:08:38 -0600

    finished exercise for this section

--- 06-The-Power-of-Functions/01-IIFE/app.js
+++ 06-The-Power-of-Functions/01-IIFE/app.js
@@ -12,12 +12,7 @@ powerDecl(4);
 powerExp(5);
 
 // IIFE - Immediately Invoked Function Expression - It is a function that is executed right after it is created and it is not accessible after it is executed. It is a good way to protect the scope of your function and the variables within it. It is also a good way to avoid polluting the global scope.
-//Example 1
-(function (num) {
-  console.log(num * num);
-})(3);
-
-//Example 2
+//Example
 (function (num) {
   console.log(num * num);
 })(3);
